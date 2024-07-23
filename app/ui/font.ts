de65239e2f371178a2f8f1af63ec0f80b4ab1920
yashika <mehndirattayashika5@gmail.com>
2024-07-23 16:04:03 +0530

    changed file

---
+++ app/ui/font.ts
@@ -0,0 +1,4 @@
+import { Inter } from 'next/font/google';
+import {lucitana} from 'next/font/google'
+export const inter = Inter({ subsets: ['latin'] });
+export const lucitana= lucitana({weight:"400",subsets:['latin']})
