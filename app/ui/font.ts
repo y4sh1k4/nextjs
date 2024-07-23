8423636053b954a301f79991bff36a38578e1e05
yashika <mehndirattayashika5@gmail.com>
2024-07-23 16:05:26 +0530

    changed font name

--- app/ui/font.ts
+++ app/ui/font.ts
@@ -1,4 +1,4 @@
 import { Inter } from 'next/font/google';
-import {lucitana} from 'next/font/google'
+import {Lusitana} from 'next/font/google'
 export const inter = Inter({ subsets: ['latin'] });
-export const lucitana= lucitana({weight:"400",subsets:['latin']})
+export const lucitana= Lusitana({weight:"400",subsets:['latin']})
